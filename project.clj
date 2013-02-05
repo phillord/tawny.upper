2d552dbf9f1aa69a77bb4e99c5737877dabac97d
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-02-05 10:53:00 +0000

    Initial Commit.

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject tawny-upper "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+
+  :license {:name "LGPL"
+            :url "http://www.gnu.org/licenses/lgpl-3.0.txt"
+            :distribution :repo}
+
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [uk.org.russet/tawny-owl "0.9-SNAPSHOT"]
+                 ])
