70ed20838c4d281c700a03f9cbdbdb036077623c
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-02-18 22:39:26 +0000

    Documentation added. save-ontology forms moved to core.

--- src/tawny/upper/annotate.clj
+++ src/tawny/upper/annotate.clj
@@ -1,4 +1,4 @@
-;; The contents of this file are subject to the GPL License, Version 3.0.
+;; The contents of this file are subject to the LGPL License, Version 3.0.
 ;; 
 ;; Copyright (C) 2013, Phillip Lord, Newcastle University
 ;; 
@@ -19,6 +19,13 @@
 (ns tawny.upper.annotate
   (:use [tawny.owl]))
 
+;; ## Introduction
+;;
+;; These are a set of annotation property who purposes is to provide a more
+;; structured representation for the documentation. Their intention is for use
+;; within the tawny.upper upper ontology design patterns; however they are not
+;; import these ontologies, to ensure that they are usuable independentally.
+
 (defontology annotate
   :iri "http://www.russet.org.uk/annotate"
   :prefix "annotate:"
@@ -30,18 +37,15 @@
 ;; that is a definition that an experiment in the domain would be likely to
 ;; come up with. Scope is a way of describing restrictions to these
 ;; interpretations; in essence, it a mechanism for the ontology developer to
-;; say, that more complex definitions have already been considered
-
-
+;; say, that more complex definitions have already been considered, but are
+;; being ignored or simplified. 
 (defannotationproperty Scope
   :comment "The scope provides context to the definition and describes that
   parts of the domain which are (or are not) intended to be described by definition." )
 
+;; The scope function is a convienient function to apply the Scope property
+;; above.
 (def scope
   (partial tawny.owl/annotation Scope))
 
 
-
-
-(save-ontology "annotate.owl" :owl)
-(save-ontology "annotate.omn" :omn)
